651159b99972a133c75eed0e288cb22f58ddfbd7
itsjulian <julian@salesgod.com>
2022-11-29 01:12:13 +0500

    Auth0 has been configured + auth issues resolved.

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
-import { AuthContext } from '../contexts/jwt-context';
+// import { AuthContext } from '../contexts/jwt-context';
+import { AuthContext } from '../contexts/auth0-context';
 
 export const useAuth = () => useContext(AuthContext) as any;
